1e275b9e934a422df4a24e4cbae77fad45212d4f
Achim Kuntz <achimobil@hotmail.com>
2023-06-25 15:58:53 +0200

    Dialog verbessert und icon eingefügt

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,6 +32,7 @@ namespace BakeTool
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             button1 = new Button();
             btnSelectAO = new Button();
             labelSelectedAO = new Label();
@@ -45,28 +46,28 @@ namespace BakeTool
             // 
             // button1
             // 
-            button1.Location = new Point(98, 173);
+            button1.Location = new Point(12, 117);
             button1.Name = "button1";
-            button1.Size = new Size(94, 29);
+            button1.Size = new Size(206, 29);
             button1.TabIndex = 0;
-            button1.Text = "Save Specular";
+            button1.Text = "Combine and save as";
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
             // btnSelectAO
             // 
-            btnSelectAO.Location = new Point(98, 68);
+            btnSelectAO.Location = new Point(12, 12);
             btnSelectAO.Name = "btnSelectAO";
-            btnSelectAO.Size = new Size(94, 29);
+            btnSelectAO.Size = new Size(206, 29);
             btnSelectAO.TabIndex = 1;
-            btnSelectAO.Text = "SelectAO";
+            btnSelectAO.Text = "Select AO (green)";
             btnSelectAO.UseVisualStyleBackColor = true;
             btnSelectAO.Click += btnSelectAO_Click;
             // 
             // labelSelectedAO
             // 
             labelSelectedAO.AutoSize = true;
-            labelSelectedAO.Location = new Point(204, 74);
+            labelSelectedAO.Location = new Point(224, 16);
             labelSelectedAO.Name = "labelSelectedAO";
             labelSelectedAO.Size = new Size(42, 20);
             labelSelectedAO.TabIndex = 2;
@@ -79,7 +80,7 @@ namespace BakeTool
             // labelSelectedDirt
             // 
             labelSelectedDirt.AutoSize = true;
-            labelSelectedDirt.Location = new Point(204, 109);
+            labelSelectedDirt.Location = new Point(224, 51);
             labelSelectedDirt.Name = "labelSelectedDirt";
             labelSelectedDirt.Size = new Size(42, 20);
             labelSelectedDirt.TabIndex = 4;
@@ -87,18 +88,18 @@ namespace BakeTool
             // 
             // btnSelectDirt
             // 
-            btnSelectDirt.Location = new Point(98, 103);
+            btnSelectDirt.Location = new Point(12, 47);
             btnSelectDirt.Name = "btnSelectDirt";
-            btnSelectDirt.Size = new Size(94, 29);
+            btnSelectDirt.Size = new Size(206, 29);
             btnSelectDirt.TabIndex = 3;
-            btnSelectDirt.Text = "SelectDirt";
+            btnSelectDirt.Text = "Select Dirt/Metal (blue)";
             btnSelectDirt.UseVisualStyleBackColor = true;
             btnSelectDirt.Click += btnSelectDirt_Click;
             // 
             // labelSelectedWear
             // 
             labelSelectedWear.AutoSize = true;
-            labelSelectedWear.Location = new Point(204, 144);
+            labelSelectedWear.Location = new Point(224, 86);
             labelSelectedWear.Name = "labelSelectedWear";
             labelSelectedWear.Size = new Size(42, 20);
             labelSelectedWear.TabIndex = 6;
@@ -106,11 +107,11 @@ namespace BakeTool
             // 
             // btnSelectWear
             // 
-            btnSelectWear.Location = new Point(98, 138);
+            btnSelectWear.Location = new Point(12, 82);
             btnSelectWear.Name = "btnSelectWear";
-            btnSelectWear.Size = new Size(94, 29);
+            btnSelectWear.Size = new Size(206, 29);
             btnSelectWear.TabIndex = 5;
-            btnSelectWear.Text = "SelectWear";
+            btnSelectWear.Text = "Select Wear / Smooth (red)";
             btnSelectWear.UseVisualStyleBackColor = true;
             btnSelectWear.Click += btnSelectWear_Click;
             // 
@@ -124,7 +125,7 @@ namespace BakeTool
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(815, 264);
+            ClientSize = new Size(799, 164);
             Controls.Add(labelSelectedWear);
             Controls.Add(btnSelectWear);
             Controls.Add(labelSelectedDirt);
@@ -132,7 +133,7 @@ namespace BakeTool
             Controls.Add(labelSelectedAO);
             Controls.Add(btnSelectAO);
             Controls.Add(button1);
-            MaximizeBox = false;
+            Icon = (Icon)resources.GetObject("$this.Icon");
             MinimizeBox = false;
             Name = "Form1";
             Text = "Bake Tool by Achimobil";
